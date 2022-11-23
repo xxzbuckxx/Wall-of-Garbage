a86772fefe98beb85b1f5ecea57fe5dcdb6633a7
Zachary Traczyk <cookies951@gmail.com>
2022-11-22 20:24:13 -0800

    frontend api hit added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import react, { useEffect, useState } from "react";
+import axios from "axios";
+
+import "./App.css";
+
+type Message = {
+  id: number;
+  message: string;
+};
+
+type GetMessagesResponse = {
+  data: Message[];
+};
+
+const getMessages = async () => {
+  try {
+    const { data, status } = await axios.get<GetMessagesResponse>(
+      import.meta.env.VITE_VERCEL_API,
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    console.log(JSON.stringify(data, null, 4));
+
+    console.log("response status is: ", status);
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error mesage: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An unexcpected error occurred";
+    }
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState<GetMessagesResponse | string>({
+    data: [],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getMessages();
+      setMessages(data);
+    };
+
+    fetchData().catch(console.error);
+  }, []);
+
+  console.log(messages);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Wall of Garbage</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
